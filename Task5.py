a6c77531653d9fe8e309b6f3d202d1e7166a4626
virus <koenig.vitalij@web.de>
2020-10-26 21:48:30 +0100

    Adding function "my_reduce"

--- Task5.py
+++ Task5.py
@@ -5,19 +5,27 @@
 Подсказка: использовать функцию reduce().
 """
 from functools import reduce
+from my_functions import my_reduce
 
 my_list = [el for el in range(100, 1001, 2)]
 
-print('#' * 20)
-print('1 вариант')
-print(reduce(lambda el, result: result * el, my_list))
 
-print('#' * 20)
-print('2 вариант')
+def my_reduce(func, param):
+    result = param[0]
+    for el in param[1:]:
+        result = func(result, el)
+    return result
 
 
 def my_func(pre_el, el):
     return pre_el * el
 
 
-print(reduce(my_func, my_list))
+print('#' * 20)
+print('1 вариант')
+print(reduce(lambda el, result: result * el, my_list))
+
+
+print('#' * 20)
+print('2 вариант')
+print(my_reduce(my_func, my_list))
